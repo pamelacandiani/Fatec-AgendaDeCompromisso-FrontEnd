72a3b848add2e08ca3aa4c9ddaae21e7c2dded89
Pamela Candiani <pamela.candianipcs@gmail.com>
2024-12-09 17:47:11 -0300

    criando componentes e suas rotas

---
+++ src/app/agendas/agendas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendas',
+  templateUrl: './agendas.component.html',
+  styleUrl: './agendas.component.css'
+})
+export class AgendasComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgendasComponent } from './agendas/agendas.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendasComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
